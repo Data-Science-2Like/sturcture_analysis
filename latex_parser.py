71e51af16d4759b89d6b85ebca71a00e32f3c578
DanialP <32634882+DanialP@users.noreply.github.com>
2021-12-08 12:48:02 +0100

    added template check

--- latex_parser.py
+++ latex_parser.py
@@ -1,14 +1,30 @@
 from TexSoup import TexSoup
 import logging
 import os
+import datetime
+import re
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.INFO, filename="parser_output.log")
 logger = logging.getLogger(__name__)
 
+logger.info("========================================================================")
+logger.info("Start logging")
+logger.info(datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
 # Set up directory for LaTex Input
 directory = "Tex Files"
 texfiles = []
+template_list = [
+    "Introduction", 
+    "Related Work", 
+    "Methods", 
+    "Models", 
+    "Experimental Apparatus", 
+    "Results",
+    "Discussion",
+    "Conclusion"
+    ]
+
 
 logger.info("Append LaTex Files to List")
 # Iterate over files in directory
@@ -23,40 +39,52 @@ for filename in os.listdir(directory):
 # TODO file input muss robuster werden
 logger.info("Load LaTex File into Soup")
 # Create Dictionary to store soup objects
-soup_dicct = {}
+soup_dict = {}
 for i,item in enumerate(texfiles):
     soup = TexSoup(open(item))
     logger.debug(soup)
-    soup_dicct[i] = soup
-logger.debug(soup_dicct)
+    # TODO Use title as variable name?? is this usefull??
+    # titel = soup.find('title')
+    # key = titel.string
+    soup_dict[i] = soup
+logger.debug(soup_dict)
 logger.info("Soup objects successfully loaded into dicctionary.")
-logger.info(f"Dicctionary Size: {len(soup_dicct)}")
+logger.info(f"Dicctionary Size: {len(soup_dict)}")
+
+
+
+logger.info("Find elements in Tex file")
+for soup in soup_dict.values():
+    section_list = list(soup.find_all('section'))
+    logger.info(section_list)
+    for i, x in enumerate(section_list):
+        clean_string = re.sub('[^A-Za-z0-9 ]+', '', x.string)
+        if clean_string == template_list[i]:
+            print(True)
+        else:
+            print(False)
+
 
 
 
 # Find Elements in LaTex File --------------------
 # Load complete Document into "alls"
-alls = soup.all
-logger.debug(alls)
+# alls = soup.all
+# logger.debug(alls)
 # for x in range(20):
 #     print(alls[x])
 
-logger.info("Find elements in Tex file")
-section_list = list(soup.find_all('section'))
-# logger.debug(section_list)
-# for x in section_list: 
-#     print(x)
 
-subsection_list = list(soup.find_all('subsection'))
-# logger.debug(subsection_list)
+# subsection_list = list(soup.find_all('subsection'))
+# # logger.debug(subsection_list)
  
-abstract = soup.find('abstract')
-# logger.debug(abstract)
+# abstract = soup.find('abstract')
+# # logger.debug(abstract)
 
-citations_list = list(soup.find_all('cite'))
-# logger.debug(citations_list)
-# for x in citations_list:
-#     print(x)
+# citations_list = list(soup.find_all('cite'))
+# # logger.debug(citations_list)
+# # for x in citations_list:
+# #     print(x)
 
 
 
