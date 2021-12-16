5610e67480f71c2a748b2c0773eab6e9f7290919
DanialP <podjavorsekd@gmail.com>
2021-12-16 04:06:50 -0800

    Added Section Heading Parser

--- latex_parser.py
+++ latex_parser.py
@@ -3,16 +3,17 @@ import logging
 import os
 import datetime
 import re
+import json
 
 # Set up logging
-logging.basicConfig(level=logging.INFO, filename="parser_output.log")
+logging.basicConfig(level=logging.DEBUG, filename="Logs/parser_output.log")
 logger = logging.getLogger(__name__)
 
 logger.info("========================================================================")
 logger.info("Start logging")
 logger.info(datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
 # Set up directory for LaTex Input
-directory = "Tex Files"
+directory = "Latex2"
 texfiles = []
 template_list = [
     "Introduction", 
@@ -42,68 +43,111 @@ logger.info("Load LaTex File into Soup")
 soup_dict = {}
 # Iterate through all TexFiles and place them in dict
 for i,item in enumerate(texfiles):
-    soup = TexSoup(open(item))
-    logger.debug(soup)
-    # TODO Use title as variable name?? is this usefull??
-    # titel = soup.find('title')
-    # key = titel.string
-    soup_dict[i] = soup
-logger.debug(soup_dict)
+    try:
+        soup = TexSoup(open(item))
+        # logger.debug(soup)
+        # TODO Use title as variable name?? is this usefull??
+        # titel = soup.find('title')
+        # key = titel.string
+        soup_dict[i] = soup
+    except TypeError:
+        logger.debug("TypeError:")
+        logger.debug(f"Doc #{i}: {item} could not be loaded.")
+    except:
+        logger.debug(f"Doc #{i}: {item} could not be loaded.")
+    
+#logger.debug(soup_dict)
 logger.info("Soup objects successfully loaded into dicctionary.")
 logger.info(f"Dicctionary Size: {len(soup_dict)}")
 
 
+section_dict = {
+    0 : [],
+    1 : [],
+    2 : [],
+    3 : [],
+    4 : [],
+    5 : [],
+    6 : [],
+    7 : [],
+    8 : [],
+    9 : [],
+    10 : [],
+    11 : [],
+    12 : [],
+    13 : [],
+    14 : [],
+    15 : [],
+    16 : [],
+    17 : [],
+    18 : [],
+    19 : [],
+    20 : []
+}
 
 logger.info("Find elements in Tex file")
 # Iterate through every soup object and extract Section Headings
 for soup in soup_dict.values():
     section_list = list(soup.find_all('section'))
     logger.info(section_list)
+
+
     # Iterate through all Section Headings and compare to template list
     for i, x in enumerate(section_list):
         clean_string = re.sub('[^A-Za-z0-9 ]+', '', x.string)
+        section_dict[i].append(clean_string)
+
         #print(set(clean_string).intersection(set(template_list)))
         
         # if clean_string.lower() == template_list[i].lower():
         #     print(True, "|" ,template_list[i], " -:- ", clean_string)
         # else:
         #     print(False, "|" , template_list[i], " -:- ", clean_string)
-        print(f"Section heading: {clean_string}")
-        if clean_string.lower() == "introduction":
-            print(f"Match found: {clean_string}")
-        
-        elif clean_string.lower() == "related work":
-            print(f"Match found: {clean_string}")
- 
-        # Methodology added
-        elif clean_string.lower() == "methods" or clean_string.lower() == "methodology":
-            print(f"Match found: {clean_string}")
- 
-        elif clean_string.lower() == "models":
-            print(f"Match found: {clean_string}")
- 
-        elif clean_string.lower() == "experimental apparatus" or clean_string.lower() == "experiments":
-            print(f"Match found: {clean_string}")
- 
-        elif clean_string.lower() == "results":
-            print(f"Match found: {clean_string}")
- 
-        elif clean_string.lower() == "discussison":
-            print(f"Match found: {clean_string}")
- 
-        elif clean_string.lower() == "conclusion" or clean_string.lower() == "conclusion and future work":
-            print(f"Match found: {clean_string}")
-        
-        # Acknowledgement added
-        elif clean_string.lower() == "acknowledgement":
-            print(f"Match found: {clean_string}")
 
-        else:
-            print(f"No match found: {clean_string}")
-        
-        print("\n")
 
-    print("=====================================================")
+        # print(f"Section heading: {clean_string}")
+        # if clean_string.lower() == "introduction":
+        #     print(f"Match found: {clean_string}")
+        
+        # elif clean_string.lower() == "related work":
+        #     print(f"Match found: {clean_string}")
+ 
+        # # Methodology added
+        # elif clean_string.lower() == "methods" or clean_string.lower() == "methodology":
+        #     print(f"Match found: {clean_string}")
+ 
+        # elif clean_string.lower() == "models":
+        #     print(f"Match found: {clean_string}")
+ 
+        # elif clean_string.lower() == "experimental apparatus" or clean_string.lower() == "experiments":
+        #     print(f"Match found: {clean_string}")
+ 
+        # elif clean_string.lower() == "results":
+        #     print(f"Match found: {clean_string}")
+ 
+        # elif clean_string.lower() == "discussison":
+        #     print(f"Match found: {clean_string}")
+ 
+        # elif clean_string.lower() == "conclusion" or clean_string.lower() == "conclusion and future work":
+        #     print(f"Match found: {clean_string}")
+        
+        # # Acknowledgement added
+        # elif clean_string.lower() == "acknowledgement":
+        #     print(f"Match found: {clean_string}")
+
+        # else:
+        #     print(f"No match found: {clean_string}")
+        
+        # print("\n")
+
+    # print("=====================================================")
+    # print(section_dict)
+
+
+# Write Sections to JSON File
+json_data = json.dumps(section_dict, indent = 4)
+with open("section_headings.json", "w") as outfile:
+    outfile.write(json_data)
 
 
 
