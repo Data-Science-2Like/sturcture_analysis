cd8d60c5fce5a54ec4fa6041be019597f2a391de
DanialP <podjavorsekd@gmail.com>
2022-02-07 08:39:25 -0800

    Added support method

--- structured_data_extraction.py
+++ structured_data_extraction.py
@@ -14,18 +14,19 @@ logger.info("===================================================================
 logger.info("Start logging")
 logger.info(datetime.datetime.now().strftime("%d.%b %Y %H:%M:%S"))
 # Set up directory for LaTex Input
-directory = "Latex_big"
+directory = "Latex"
+csv_filename = "CSV/sections_headings.csv"
 texfiles = []
 
 
-logger.info("Append LaTex Files to List")
-# Iterate over files in directory
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
-    # Checking if it is a file
-    if os.path.isfile(f):
-        logger.debug(f)
-        texfiles.append(f)
+# logger.info("Append LaTex Files to List")
+# # Iterate over files in directory
+# for filename in os.listdir(directory):
+#     f = os.path.join(directory, filename)
+#     # Checking if it is a file
+#     if os.path.isfile(f):
+#         logger.debug(f)
+#         texfiles.append(f)
 
 # Load File into Soup
 logger.info("Load LaTex File into Soup")
@@ -86,11 +87,16 @@ def load_into_csv_file():
             except Exception as e:
                 logger.debug(f"Doc #{i}: {item} could not be loaded.")
                 logger.debug("Error occured: "+ str(e))
+
+def load_items_from_csv():
+    # Read out csv file
+    with open(csv_filename, 'r') as csvfile:
+            # Creating csv writer object
+            csvreader = csv.reader(csvfile)
+
+            for item in csvreader:
+                D.append(item)
             
-
-
-
-        
 
 def find_nonsense_paper():
     for i, item in enumerate(D):
@@ -113,50 +119,81 @@ def find_nonsense_paper():
         logger.debug("==================================================================")
 
 
+# Alles nach Appendix wegwerfen
+# Support Methode > auf wie viele Paper matched es
+# Abweichung bei Regeln zulassen (1 Sektion)
+
+def support_method(rule):
+    percentage = 0
+    sections = []
+    for docs in D:
+        for items in docs:
+            if "Latex" in items:
+                continue
+            if len(items) == 0:
+                continue
+            sections.append(items)
+        if rule == sections:
+            logger.debug("Rule found: ")
+            logger.debug(sections)
+            percentage += 1
+        sections = []
+    percentage = percentage / len(D)
+    #return round(percentage, 2)
+    return percentage
+
 def iter_through_doc_set():
     # loop until D is empty
     logger.debug("Size of Corpus : {}".format(len(D)))
-    while(D):
-        sections = []
-        item = D[0]
-        section_list = list(item.find_all('section'))
-        del D[0]
-        for item in section_list:
-            clean_string = re.sub('[^A-Za-z0-9 ]+', '', item.string)
-            clean_string = clean_string.lower()   
-            # TODO tokenize, lemmatize strings
-            sections.append(clean_string)
-        logger.debug("New Sections: {}".format(sections))
-        append = False
+    r = []
+    for docs in D:
+        for items in docs:
+            if "Latex" in items:
+                continue
+            if len(items) == 0:
+                continue
+            r.append(items)
+            
+        if r in R:
+            r = []
+            continue
+        #print("Supp Med: " ,support_method(r))
+        R.append(r)
+        r = []
 
-        if not R:
-            R.append(sections)
-            logger.debug("(1st) New Rule added: {}".format(sections))
-        else:
-            # R = [[r1],[r2], ...]
-            for lists in R:
-                # r1 = [I, R, M, E]
-                for items in lists:
-                    # l1 = [I, D, M, E]
-                    for items2 in sections:
-                        if items == items2:
-                            print("Same : ", items)
-                            logger.debug("Same section heading: "+ items)
-                            break
-                        else:
-                            logger.debug("Append = True")
-                            append = True                  
-            if append == True:
-                R.append(sections)
-                logger.debug("New Rule added: {}".format(sections))
-                append = False
-            else:
-                logger.debug("No New Rule added.")
+        # sections = []
+        # for item in D:
+        #     clean_string = re.sub('[^A-Za-z0-9 ]+', '', item.string)
+        #     clean_string = clean_string.lower()   
+        #     # TODO tokenize, lemmatize strings
+        #     sections.append(clean_string)
+        # logger.debug("New Sections: {}".format(sections))
+        # append = False
+
+        # if not R:
+        #     R.append(sections)
+        #     logger.debug("(1st) New Rule added: {}".format(sections))
+        # else:
+        #     # R = [[r1],[r2], ...]
+        #     for lists in R:
+        #         # r1 = [I, R, M, E]
+        #         for items in lists:
+        #             # l1 = [I, D, M, E]
+        #             for items2 in sections:
+        #                 if items == items2:
+        #                     print("Same : ", items)
+        #                     logger.debug("Same section heading: "+ items)
+        #                     break
+        #                 else:
+        #                     logger.debug("Append = True")
+        #                     append = True                  
+        #     if append == True:
+        #         R.append(sections)
+        #         logger.debug("New Rule added: {}".format(sections))
+        #         append = False
+        #     else:
+        #         logger.debug("No New Rule added.")
         
-
-
-
-
 
     logger.debug("Size of Rule Set : {}".format(len(R)))
     logger.debug("Complete Rule Set: {}".format(R))
@@ -167,8 +204,9 @@ def iter_through_doc_set():
 
 # Main function calls ===================================================
 #load_items_into_soup()
-load_into_csv_file()
-#iter_through_doc_set()
+#load_into_csv_file()
+load_items_from_csv()
+iter_through_doc_set()
 #find_nonsense_paper()
 
 
