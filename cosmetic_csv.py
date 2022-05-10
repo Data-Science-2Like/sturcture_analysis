938283696490673189f9452b4d7d4dcb43a7682e
DanialP <podjavorsekd@gmail.com>
2022-05-10 08:41:16 -0700

    Update

--- cosmetic_csv.py
+++ cosmetic_csv.py
@@ -1,8 +1,8 @@
 import csv
 import logging
 
-in_filename = "CSV/sections_headings_v2.csv"
-out_filename = "CSV/sections_headings_v2_test.csv"
+in_filename = "CSV/lemmatizer/sections_16k_lemma.csv"
+out_filename = "CSV/lemmatizer/sections_16k_lemma_v2.csv"
 new_list = []
 
 # Set up logging
@@ -26,7 +26,11 @@ with open(out_filename, 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
 
     for list in section_list:
+        #print(len(list))
+        if len(list) < 2:
+            continue
         for item in list:
+            # print(item)
             if len(item) > 0:
                 new_list.append(item)
 
